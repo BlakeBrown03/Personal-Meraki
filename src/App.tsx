495f0cdaf1c07e29069e0f066ec30ba7e34b03ea
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-11-13 12:57:12 -0600

    Starting to map all the networks into cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Button from "react-bootstrap/Button";
+import NetworkCard from "./NetworkCard";
 
 function App() {
-	const [data, setData] = useState([]);
+	const [networks, setNetworks] = useState([]);
 
 	async function fetchData() {
-		const response = await fetch("http://localhost:3000/https://api.meraki.com/api/v1/organizations", {
-    headers: {
-        "X-Cisco-Meraki-API-Key": "e1613b45cd4d8fe315ce194c039381176091a534"
-    }
-});
+		const response = await fetch(
+			"http://localhost:3000/https://api.meraki.com/api/v1/organizations/289024/networks",
+			{
+				headers: {
+					"X-Cisco-Meraki-API-Key":
+						"e1613b45cd4d8fe315ce194c039381176091a534"
+				}
+			}
+		);
 		const respData = await response.json();
-		setData(respData);
+		setNetworks(respData);
 		console.log(respData);
 	}
 
@@ -22,7 +26,8 @@ function App() {
 
 	return (
 		<>
-			<Button>Hello</Button>
+			<h1 style={{textAlign: "center"}}>Welcome to Meraki API</h1>
+			{networks.map((network: any) => <NetworkCard id={network.id}>{...network}</NetworkCard>)}
 		</>
 	);
 }
