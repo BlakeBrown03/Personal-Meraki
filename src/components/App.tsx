134b90fb601cc5c35284933a58924e595afb3097
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-11-21 10:31:52 -0600

    Added basic pagination

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import NetworkCard from "./NetworkCard";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Pagination, Row } from "react-bootstrap";
 
 function App() {
 	const [networks, setNetworks] = useState([]);
+	const [shownNetworks, setShownNetworks] = useState([]);
+	const [page, setPage] = useState(1);
 
 	async function fetchData() {
 		const response = await fetch(
@@ -21,6 +23,22 @@ function App() {
 		// console.log(respData);
 	}
 
+	function buildPaginator() {
+		const pages: number = Math.ceil(networks.length / 24);
+		const paginator = [];
+		for (let i = 1; i <= pages; i++) {
+			paginator.push(
+				<Pagination.Item
+					key={i}
+					active={page === i}
+					onClick={() => setPage(i)}>
+					{i}
+				</Pagination.Item>
+			);
+		}
+		return paginator;
+	}
+
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -31,20 +49,26 @@ function App() {
 			{networks.length > 0 ? (
 				<Container fluid>
 					<Row>
-						{networks.map((network: any) => (
-							<Col
-								style={{ paddingTop: 10, paddingBottom: 10 }}
-								key={network.id}
-								md={12}
-								xl={6}>
-								<NetworkCard {...network}></NetworkCard>
-							</Col>
-						))}
+						{networks
+							.slice(24 * (page - 1), 24 * page)
+							.map((network: any) => (
+								<Col
+									style={{
+										paddingTop: 10,
+										paddingBottom: 10
+									}}
+									key={network.id}
+									md={12}
+									xl={6}>
+									<NetworkCard {...network}></NetworkCard>
+								</Col>
+							))}
 					</Row>
 				</Container>
 			) : (
 				<h1 style={{ textAlign: "center" }}>Loading...</h1>
 			)}
+			<Pagination>{buildPaginator()}</Pagination>
 		</>
 	);
 }
