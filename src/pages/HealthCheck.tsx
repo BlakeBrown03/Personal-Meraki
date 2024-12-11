11c01cf94e33b8ce0f51d5bf1719d9526731045e
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-11 10:30:25 -0600

    Enhance HealthCheck component to display current date and improve table styling with bold headers and conditional row coloring

--- src/pages/HealthCheck.tsx
+++ src/pages/HealthCheck.tsx
@@ -4,29 +4,80 @@ import { useEffect, useState } from "react";
 
 export default function HealthCheck() {
 	const [devices, setDevices] = useState<any[]>([]);
+	const [date, setDate] = useState<string>("");
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	];
 
 	useEffect(() => {
 		setDevices(devicesEOD);
+		const currDate = new Date();
+		setDate(
+			`${currDate.getDate()}-${months[currDate.getMonth()]}-${String(
+				currDate.getFullYear()
+			).substring(2)}`
+		);
 	}, []);
 
 	return (
 		<>
 			<Container fluid>
-				<Table>
+				<Table bordered>
 					<thead>
 						<tr>
-							<td>Device</td>
-							<td>End of Support Date</td>
-							<td>End of Sale Date</td>
-							<td>EOS PID</td>
-							<td>Replacement Product</td>
+							<th>
+								<b>
+									<i>Device</i>
+								</b>
+							</th>
+							<th>
+								<b>
+									<i>End of Support Date</i>
+								</b>
+							</th>
+							<th>
+								<b>
+									<i>End of Sale Date</i>
+								</b>
+							</th>
+							<th>
+								<b>
+									<i>EOS PID</i>
+								</b>
+							</th>
+							<th>
+								<b>
+									<i>Replacement Product</i>
+								</b>
+							</th>
 						</tr>
 					</thead>
 					<tbody>
 						{devices.map((device: any, index: number) => {
 							const key: string = Object.keys(device)[0];
+							let currColor: string = "";
+							date.substring(0, 2) >
+							device[key]["End-of-Support Date"].substring(0, 2)
+								? (currColor = "red")
+								: (currColor = "green");
+							console.log(currColor);
 							return (
-								<tr key={`${key}-${index}`}>
+								<tr
+									key={`${key}-${index}`}
+									style={{
+										backgroundColor: "black"
+									}}>
 									<td>{key}</td>
 									<td>
 										{device[key]["End-of-Support Date"]}
