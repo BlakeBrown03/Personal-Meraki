153f0c524b71f870d13a92009e1731f354294257
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-11 09:50:35 -0600

    Implement HealthCheck component to display device information in a table format

--- src/pages/HealthCheck.tsx
+++ src/pages/HealthCheck.tsx
@@ -1,14 +1,47 @@
-import { Accordion, Col, Container, Row } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
+import devicesEOD from "../../your-json.json";
+import { useEffect, useState } from "react";
 
-export default function HealthCheck(props: any) {
-    return <>
-        <Container>
-            <Row>
-                <Col>
-                    <Accordion defaultActiveKey="0">
-                    </Accordion>
-                </Col>
-            </Row>
-        </Container>
-    </>
-};
+export default function HealthCheck() {
+	const [devices, setDevices] = useState<any[]>([]);
+
+	useEffect(() => {
+		setDevices(devicesEOD);
+	}, []);
+
+	return (
+		<>
+			<Container fluid>
+				<Table>
+					<thead>
+						<tr>
+							<td>Device</td>
+							<td>End of Support Date</td>
+							<td>End of Sale Date</td>
+							<td>EOS PID</td>
+							<td>Replacement Product</td>
+						</tr>
+					</thead>
+					<tbody>
+						{devices.map((device: any, index: number) => {
+							const key: string = Object.keys(device)[0];
+							return (
+								<tr key={`${key}-${index}`}>
+									<td>{key}</td>
+									<td>
+										{device[key]["End-of-Support Date"]}
+									</td>
+									<td>{device[key]["End-of-Sale Date"]}</td>
+									<td>{device[key]["EOS PID"]}</td>
+									<td>
+										{device[key]["Replacement-Product"]}
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			</Container>
+		</>
+	);
+}
