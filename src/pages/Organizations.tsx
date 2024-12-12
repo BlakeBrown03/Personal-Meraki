0d3ea32f44791f7f918f185224c75b55cbad203b
BlakeBrown03 <blake.m.brown03@gmail.com>
2024-12-12 13:05:18 -0600

    Enhance Organizations page by adding table display for organizations and updating API request headers

--- src/pages/Organizations.tsx
+++ src/pages/Organizations.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 
 export default function Organizations() {
 	const [organizations, setOrganizations] = useState<Record<string, any>>([]);
@@ -11,7 +11,8 @@ export default function Organizations() {
 			{
 				method: "GET",
 				headers: {
-					"X-Cisco-Meraki-API-Key": apiKey
+					"X-Cisco-Meraki-API-Key": apiKey,
+					"Content-Type": "application/json"
 				}
 			}
 		);
@@ -25,8 +26,20 @@ export default function Organizations() {
 			{
 				method: "POST",
 				headers: {
-					"X-Cisco-Meraki-API-Key": apiKey
-				}
+					"X-Cisco-Meraki-API-Key": apiKey,
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					name: "Test Organization",
+					management: {
+						details: [
+							{
+								name: "Test1",
+								value: "12345"
+							}
+						]
+					}
+				})
 			}
 		);
 		const data = await response.json();
@@ -35,7 +48,8 @@ export default function Organizations() {
 
 	useEffect(() => {
 		fetchOrganizations();
-	}, []);
+	}, [organizations]);
+
 	return (
 		<div style={{ textAlign: "center" }}>
 			<h1>Organizations</h1>
@@ -43,7 +57,20 @@ export default function Organizations() {
 			{organizations["management"] === undefined ? (
 				<h5>It looks like you do not have any organizations yet</h5>
 			) : (
-				<></>
+				organizations["management"].map((organization: any) => {
+					return (
+						<Table key={organization.id}>
+							<tbody>
+								<tr>
+									<td>
+										<b>Name:</b>
+									</td>
+									<td>{organization.name}</td>
+								</tr>
+							</tbody>
+						</Table>
+					);
+				})
 			)}
 		</div>
 	);
